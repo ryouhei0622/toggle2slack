0b2e0872cae02100ec696b3181218b1b4ab07a8c
ryouhei0622 <seijakunomutou@gmail.com>
2023-06-11 17:45:25 +0900

    refac

--- toggle2slack.sh
+++ toggle2slack.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source ~/miniforge3/etc/profile.d/conda.sh
-cd ~/code/utilities/ && conda init zsh && conda activate utilities && python toggle2slack.py
+cd ~/code/utilities/ && touch error.log && touch out.log && conda init zsh && conda activate utilities && python toggle2slack.py
